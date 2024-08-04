61473ea38a544622793f68d2ae6b87d77dfa6aa0
Adrian <noedigsti@gmail.com>
2024-08-04 21:47:05 +1000

    increase size

--- consumer.sh
+++ consumer.sh
@@ -2,4 +2,4 @@ docker build -t kafka-consumer ./consumer
 
 echo "Receiving messages from producer(s)"
 
-docker run --rm -it --name kafka-consumer --network project_kafka-network kafka-consumer /bin/bash -c "bin/kafka-console-consumer.sh --bootstrap-server kafka-broker:9092 --topic test --from-beginning"
+docker run --rm -it --name kafka-consumer --network cluster_kafka-network kafka-consumer /bin/bash -c "bin/kafka-console-consumer.sh --bootstrap-server kafka-broker-1:9092 --topic test --from-beginning"
--- producer.sh
+++ producer.sh
@@ -2,4 +2,4 @@ docker build -t kafka-producer ./producer
 
 echo "Type something to send to the consumer:"
 
-docker run --rm -it --name kafka-producer --network project_kafka-network kafka-producer /bin/bash -c "bin/kafka-console-producer.sh --broker-list kafka-broker:9092 --topic test"
+docker run --rm -it --name kafka-producer --network cluster_kafka-network kafka-producer /bin/bash -c "bin/kafka-console-producer.sh --broker-list kafka-broker-1:9092 --topic test"
