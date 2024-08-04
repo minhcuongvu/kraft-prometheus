74ff0fd865c92065513f1691d8e8e1442636eb47
Adrian <noedigsti@gmail.com>
2024-08-04 20:43:43 +1000

    1 cluster with 1 controller, 2 brokers

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+
+# Define the log file
+LOG_FILE="init.log"
+
 # KRaft
+# Function to log both to console and file
+log() {
+  echo "$1" | tee -a $LOG_FILE
+}
 
-# Generate a Cluster UUID
-KAFKA_CLUSTER_ID=$(bin/kafka-storage.sh random-uuid)
+# KRaft
+log "HOSTNAME=$HOSTNAME"
+log "PORT=$PORT"
+log "NODE_ID=$NODE_ID"
 
-# Format Log Directories
-bin/kafka-storage.sh format -t "$KAFKA_CLUSTER_ID" -c config/kraft/server.properties --ignore-formatted
+# Replace placeholders in server.properties
+envsubst < config/kraft/server.properties.template > config/kraft/server.properties
+
+cat config/kraft/server.properties
+
+# Log the generated Cluster UUID
+# KAFKA_CLUSTER_ID=$(bin/kafka-storage.sh random-uuid)
+KAFKA_CLUSTER_ID=3SIJT0pEQKW3veTmmhu8WA
+log "KAFKA_CLUSTER_ID=$KAFKA_CLUSTER_ID"
+
+# Format Log Directories and log the output
+bin/kafka-storage.sh format -t "$KAFKA_CLUSTER_ID" -c config/kraft/server.properties --ignore-formatted 2>&1 | tee -a $LOG_FILE
 
 # Start the Kafka Server
-KAFKA_OPTS="-javaagent:/opt/kafka/jmx_prometheus_javaagent-1.0.1.jar=7071:/opt/kafka/kafka-jmx-exporter.yml" bin/kafka-server-start.sh config/kraft/server.properties
+KAFKA_OPTS="-javaagent:/opt/kafka/jmx_prometheus_javaagent-1.0.1.jar=$PORT:/opt/kafka/kafka-jmx-exporter.yml" bin/kafka-server-start.sh config/kraft/server.properties
--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,1 +1,4 @@
-docker-compose down && docker-compose build && docker-compose -p project up -d
+# docker-compose down && docker-compose build --no-cache && docker-compose -p cluster up -d
+docker-compose -p cluster up -d --build
+
+# kafka-broker-api-versions.sh --bootstrap-server kafka-broker-1:9092
