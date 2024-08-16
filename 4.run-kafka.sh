b7c94364a0ab26f70f519fee3d22e898574032f2
Adrian <noedigsti@gmail.com>
2024-08-16 12:57:19 +1000

    update advertised listeners

--- 4.run-kafka.sh
+++ 4.run-kafka.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 kubectl apply -f kafka-controller-1.yaml
 kubectl rollout restart deployment kafka-controller-1 -n kafka
+
+kubectl apply -f kafka-broker-1.yaml
+kubectl rollout restart deployment kafka-broker-1 -n kafka
+
+kubectl apply -f prometheus-1.yaml
+kubectl rollout restart deployment prometheus-1 -n kafka
